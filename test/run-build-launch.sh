6c330449486be574d181418cdead200897a9f085
Ki-Wang Ho <kho@macbookpro-22631.lan>
2018-08-19 19:28:04 -0400

    Updated run-build-launch.sh to only perform maven (instead of gradle)

--- test/run-build-launch.sh
+++ test/run-build-launch.sh
@@ -1,14 +1,10 @@
 cd $(dirname $0)
 cd ../
 mvn clean package
-
-rm -rf target
-
-./gradlew build
 ret=$?
 if [ $ret -ne 0 ]; then
 exit $ret
 fi
 
-$JAVA_HOME/bin/java -jar build/libs/palindrome-rest-service-0.1.0.jar
+$JAVA_HOME/bin/java -jar target/palindrome-rest-service-0.1.0.jar
 exit
